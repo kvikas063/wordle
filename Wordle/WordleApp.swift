8c766be65f39c5c063b79d8b7904319f378eebc4
Vikas Kumar <vikas@myhealthcare.life>
2022-03-15 17:24:41 +0530

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Vikas Kumar on 15/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
